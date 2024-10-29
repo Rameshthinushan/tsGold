e0d6f6b7d0dd4743c515d94b64a38f54fba6be1c
Thinu <88131481+Rameshthinushan@users.noreply.github.com>
2024-10-30 00:03:25 +0530

    Update settings.jsx

--- client/app/components/settings.jsx
+++ client/app/components/settings.jsx
@@ -1,199 +1,126 @@
-import React from 'react';
 import {StyleSheet, Text, ScrollView, View, TouchableOpacity, TextInput} from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { Avatar, Divider } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useState } from 'react';
 
-const Settings = () => (
-  <SafeAreaProvider>
-    <SafeAreaView style={styles.container} edges={['top']}>
-      <ScrollView>
-        <View style={styles.searchContainer}>
-          <Icon name="magnify" size={20} color="grey" style={styles.searchIcon} />
-          <TextInput
-            style={styles.input}
-            placeholder="Search"
-            placeholderTextColor="grey"
-          />
-        </View>
+export default function Settings() { 
+  const [settingList, setSettingsList] = useState([
+    {
+      'System Settings': [{ 
+        text: 'Management', 
+        icon: 'cog', 
+        href: '#' 
+      }, { 
+        text: 'ReTake', 
+        icon: 'rotate-3d-variant', 
+        href: '#' 
+      }, { 
+        text: 'Return', 
+        icon: 'keyboard-return', 
+        href: '#' 
+      }, { 
+        text: 'Goldsimth', 
+        icon: 'account', 
+        href: '#' 
+      }, { 
+        text: 'Trash', 
+        icon: 'trash-can', 
+        href: '#' 
+      }, { 
+        text: 'Category', 
+        icon: 'dots-grid', 
+        href: '#' 
+      }, { 
+        text: 'Product Type', 
+        icon: 'menu', 
+        href: '#' 
+      } 
+    ]}, {
+      'User Settings': [{ 
+        text: 'Employee Management', 
+        icon: 'account-circle', 
+        href: '#' 
+      }]
+    }, {
+      'Notification': [{ 
+        text: 'Employee Management', 
+        icon: 'bell', 
+        href: '#' 
+      }]
+    }
+  ]);
 
-        <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingEnd: 20}}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <View>
-              <Avatar.Text size={50} label="RT" style={{
-                backgroundColor: '#091057',
-                margin: 20
-              }}/>
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={['top']}>
+        <ScrollView>
+          <View style={styles.searchContainer}>
+            <Icon name="magnify" size={20} color="grey" style={styles.searchIcon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Search"
+              placeholderTextColor="grey"
+            />
+          </View>
+
+          <View style={styles.mainBody}>
+            <View style={styles.editProfile}>
+              <View style={styles.profileContainer}>
+                <Avatar.Text size={50} label="RT" style={styles.profileAvatar} />
+                <View>
+                  <Text style={styles.nameTetx}>Thinushan.R</Text>
+                  <Text style={styles.mailTetx}>rameshthinushan@gmail.com</Text>
+                </View>
+              </View>
+              <Icon name="chevron-right" size={30} />
             </View>
+            <Divider/>
             <View>
-              <Text style={{fontSize: 20, fontWeight:'500'}}>Thinushan.R</Text>
-              <Text style={{color: 'grey'}}>rameshthinushan@gmail.com</Text>
+              {settingList.map((settings, i) => (
+                <View key={i}>
+                  {Object.keys(settings).map((key, j) => (
+                    <View key={j}>
+                      <Text style={styles.settingTitle}>{key}</Text>
+
+                      {settings[key].map((subItem, index) => (
+                        <TouchableOpacity 
+                          key={index} 
+                          style={styles.settingsItems}
+                        >
+                          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Icon 
+                              name={subItem.icon} 
+                              size={25} 
+                              style={styles.settingsListicon} 
+                            />
+                            <Text style={styles.IconText}>{subItem.text}</Text>
+                          </View>
+                          <Icon 
+                            name="chevron-right" 
+                            size={30} 
+                          />
+                        </TouchableOpacity>
+                      ))}
+                    </View>
+                  ))}
+                </View>
+              ))}
             </View>
           </View>
-          <View>
-            <Icon name="chevron-right" size={30}></Icon>
-          </View>
-        </View>
-        <Divider/>
-        <View style={{paddingStart: 20, paddingEnd: 20}}>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View>
-                <Icon name="cog" size={25} style={{margin: 10}}></Icon>
-              </View>
-              <View>  
-                <Text style={{fontSize: 15}}>Management</Text>
-              </View>
-            </View>
-            <View>
-              <Icon name="chevron-right" size={30}></Icon>
-            </View>
-          </View>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View>
-                <Icon name="cog" size={25} style={{margin: 10}}></Icon>
-              </View>
-              <View>  
-                <Text style={{fontSize: 15}}>ReTake</Text>
-              </View>
-            </View>
-            <View>
-              <Icon name="chevron-right" size={30}></Icon>
-            </View>
-          </View>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View>
-                <Icon name="cog" size={25} style={{margin: 10}}></Icon>
-              </View>
-              <View>  
-                <Text style={{fontSize: 15}}>Return</Text>
-              </View>
-            </View>
-            <View>
-              <Icon name="chevron-right" size={30}></Icon>
-            </View>
-          </View>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View>
-                <Icon name="cog" size={25} style={{margin: 10}}></Icon>
-              </View>
-              <View>  
-                <Text style={{fontSize: 15}}>Goldsmith</Text>
-              </View>
-            </View>
-            <View>
-              <Icon name="chevron-right" size={30}></Icon>
-            </View>
-          </View>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View>
-                <Icon name="cog" size={25} style={{margin: 10}}></Icon>
-              </View>
-              <View>  
-                <Text style={{fontSize: 15}}>Trash</Text>
-              </View>
-            </View>
-            <View>
-              <Icon name="chevron-right" size={30}></Icon>
-            </View>
-          </View>
-          <Divider/>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View>
-                <Icon name="cog" size={25} style={{margin: 10}}></Icon>
-              </View>
-              <View>  
-                <Text style={{fontSize: 15}}>Management</Text>
-              </View>
-            </View>
-            <View>
-              <Icon name="chevron-right" size={30}></Icon>
-            </View>
-          </View>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View>
-                <Icon name="cog" size={25} style={{margin: 10}}></Icon>
-              </View>
-              <View>  
-                <Text style={{fontSize: 15}}>Management</Text>
-              </View>
-            </View>
-            <View>
-              <Icon name="chevron-right" size={30}></Icon>
-            </View>
-          </View>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View>
-                <Icon name="cog" size={25} style={{margin: 10}}></Icon>
-              </View>
-              <View>  
-                <Text style={{fontSize: 15}}>Management</Text>
-              </View>
-            </View>
-            <View>
-              <Icon name="chevron-right" size={30}></Icon>
-            </View>
-          </View>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View>
-                <Icon name="cog" size={25} style={{margin: 10}}></Icon>
-              </View>
-              <View>  
-                <Text style={{fontSize: 15}}>Management</Text>
-              </View>
-            </View>
-            <View>
-              <Icon name="chevron-right" size={30}></Icon>
-            </View>
-          </View>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View>
-                <Icon name="cog" size={25} style={{margin: 10}}></Icon>
-              </View>
-              <View>  
-                <Text style={{fontSize: 15}}>Management</Text>
-              </View>
-            </View>
-            <View>
-              <Icon name="chevron-right" size={30}></Icon>
-            </View>
-          </View>
-          <Divider/>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  </SafeAreaProvider>
-);
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );  
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  buttonContainer: {
-    backgroundColor: '#EC8305',
-    padding: 6,
-    marginTop: 10,
-    borderRadius: 20,
-    width: 140
-  },
-  buttonText: {
-    color: 'white',
-    textAlign: 'center'
-  },
-  text: {
-    fontSize: 42,
-    padding: 12,
-  },
   searchContainer: {
+    flex: 2,
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: 'white',
@@ -207,10 +134,51 @@ const styles = StyleSheet.create({
     marginRight: 8,
   },
   input: {
-    flex: 1,
+    flex: 2,
     fontSize: 16,
     color: '#000'
   },
+  profileAvatar: {
+    backgroundColor: '#091057',
+    marginEnd: 10
+  },
+  editProfile: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'space-between', 
+    marginBottom: 10
+  },
+  mainBody: {
+    flex: 3, 
+    margin: 15
+  },
+  profileContainer: {
+    flexDirection: 'row', 
+    alignItems: 'center'
+  },
+  nameTetx: {
+    fontSize: 20, 
+    fontWeight: '500'
+  }, 
+  mailTetx: {
+    color: 'grey'
+  },
+  settingTitle: { 
+    fontSize: 16,
+    fontWeight: '500', 
+    color: 'grey',
+    marginVertical: 10, 
+  },
+  settingsItems: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'space-between', 
+    margin: 5
+  },
+  settingsListicon: {
+    marginRight: 10
+  },
+  IconText: {
+    fontSize: 16
+  }
 });
-
-export default Settings;
